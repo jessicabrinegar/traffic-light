ce3b0ca01d4b739232101997c81b4b9b30434b7d
jessicabrinegar <jessicabrinegar@outlook.com>
2023-01-19 20:44:23 +0000

    completed project

--- src/js/component/Lights.jsx
+++ src/js/component/Lights.jsx
@@ -11,12 +11,17 @@ const Lights = () => {
             setActiveColor(null);
         }
     }
-    const colors = [{id:1, color:'red'}, {id:2, color:'yellow'}, {id:3, color:'green'}]
+    const colors = ['red', 'yellow', 'green']
+    function TrafficLight({color}){
+        return (
+        <div onClick={() => ToggleClass(color)} 
+        className={`rounded-circle ${color}-light ${activeColor == color ? 'selected' : ''}`}>
+        </div>
+        )
+    }
     return(
-    <div className ="bg-dark stoplight-container py-2 px-2 w-100 d-flex flex-column align-items-center">
-        <div onClick={() => ToggleClass('red')} className={"rounded-circle red-light " + (activeColor == 'red' ? 'selected': '')}></div>
-        <div onClick={() => ToggleClass('yellow')} className={"rounded-circle yellow-light my-2 " + (activeColor == 'yellow' ? 'selected': '')}></div>
-        <div onClick={() => ToggleClass('green')} className={"rounded-circle green-light " + (activeColor == 'green' ? 'selected': '')}></div>
+    <div className ="bg-dark stoplight-container py-2 px-2 w-100 d-flex flex-column align-items-center rounded">
+        {colors.map((color) => <TrafficLight color={color}/>)}
     </div>
     )
 }

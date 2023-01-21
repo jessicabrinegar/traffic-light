6522065661fa4ee53ee4b60f174e8f19bb984f5a
jessicabrinegar <jessicabrinegar@outlook.com>
2023-01-21 00:32:52 +0000

    finished project

--- src/js/component/Lights.jsx
+++ src/js/component/Lights.jsx
@@ -3,18 +3,14 @@ import React, {useState} from "react";
 const Lights = () => {
     const [activeColor, setActiveColor] = useState(null);
 
-    function ToggleClass(color){
-        if(activeColor != color){
-            setActiveColor(color)
-        }
-        else{
-            setActiveColor(null);
-        }
+    function toggleClass(color){
+        if(activeColor != color) setActiveColor(color)
+        else setActiveColor(null);
     }
     const colors = ['red', 'yellow', 'green']
-    function TrafficLight({color}){
+    const TrafficLight = ({color}) => {
         return (
-        <div onClick={() => ToggleClass(color)} 
+        <div onClick={() => toggleClass(color)} 
         className={`rounded-circle ${color}-light ${activeColor == color ? 'selected' : ''}`}>
         </div>
         )
